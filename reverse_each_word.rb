0da110dd2e1df951f621f4fb9338748bb272a632
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-24 19:11:46 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,14 @@
 def reverse_each_word(sentence)
-  reverse_sentence = []
-  sentence.split.each do |word|
-    reverse_sentence << word.reverse
-  end
-  reverse_sentence.join(" ")
+  sentence.split.collect do |word|
+    word.reverse
+  end 
+
+
+
+
+  # reverse_sentence = []
+  # sentence.split.each do |word|
+  #   reverse_sentence << word.reverse
+  # end
+  # reverse_sentence.join(" ")
 end
